ef7c7cff75d595367a4242f6fd163fca04f84cf3
hipotecas <kisoua@outlook.com>
2021-04-05 19:17:04 +0800

    chore: update dependency * fix style import in md

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,25 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'ligero',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
-  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: 'ligero',
+        camel2DashComponentName: false,
+        customStyleName: () => {
+          return `./style/index.less`; // 注意：这里 ./ 不可省略
+        },
+      },
+      'lean',
+    ],
+  ],
+  webpack5: {},
   // more config: https://d.umijs.org/config
 });
