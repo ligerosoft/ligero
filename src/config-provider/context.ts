e027569e9cfc94e34347bf01d3ec336fcba4f3f4
kisoua <80826665+Hipotecas@users.noreply.github.com>
2021-05-17 22:33:06 +0800

    refactor: 💡 config provider prefixCls

--- src/config-provider/context.ts
+++ src/config-provider/context.ts
@@ -1,5 +1,11 @@
 import { createContext } from 'react';
 
+export const globalConfig = () => ({
+  getPrefixCls: (suffixCls?: string, customizedPrefixCls?: string) => {
+    if (customizedPrefixCls) return customizedPrefixCls;
+    return suffixCls ? `ligero-${suffixCls}` : 'ligero';
+  },
+});
 export interface ConfigConsumerProps {
   getPrefixCls: (suffixCls?: string, customizePrefixCls?: string) => string;
   theme?: 'light' | 'dark';
